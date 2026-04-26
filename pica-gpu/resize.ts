d67e5e5fa12e493de521bce5616af8e2aeac8c17
Kat <10361870+transkatgirl@users.noreply.github.com>
2026-04-25 22:28:42 -0700

    Bug fixes

--- pica-gpu/resize.ts
+++ pica-gpu/resize.ts
@@ -465,7 +465,7 @@ export class Scaler {
 		if (sync) {
 			while (signal === gl.TIMEOUT_EXPIRED) {
 				await new Promise(function (resolve) {
-					setTimeout(resolve, 0);
+					setTimeout(resolve, 1);
 				});
 
 				signal = gl.clientWaitSync(sync, 0, 0);
